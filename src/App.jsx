6b36fdf19789070ed74ed35b2f391db372a510cd
Martin Costa Daddato <costadaddato@gmail.com>
2023-02-22 20:35:47 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { TaskForm } from "./Components/TaskForm";
 
 function App() {
   return (
-    <main className="bg-slate-900 min-h-screencd">
+    <main className="bg-slate-900 min-h-screen">
       <div className="container mx-auto p-5">
         <TaskForm />
         <TaskList />
